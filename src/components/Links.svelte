5c828703b0de826a75ffc9b09432f5c3ef9a849f
fairuz <sakhiyfairuz@gmail.com>
2024-11-06 14:57:18 +0700

    added link components

---
+++ src/components/Links.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let text: string;
+    export let url: string;
+</script>
+
+<a href={url} target="_blank">
+    <div class="px-3 py-2 rounded-lg border hover:bg-gray-200">
+        {text}
+    </div>
+</a>
