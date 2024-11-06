22129b8bef55d7ead77298033a661766a6562699
fairuz <sakhiyfairuz@gmail.com>
2024-11-06 14:40:45 +0700

    added loading screen when fetching data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import axios from "axios";
   import { onMount } from "svelte";
+  import LoadingDots from "../components/LoadingDots.svelte";
 
   let myName: string;
   let myIntro: string;
   let myDescription: string;
+  let loading: boolean = true;
 
   async function getName() {
     const q = await axios({
@@ -41,35 +43,55 @@
     myDescription = q.data.data.attributes.description;
   }
 
-  onMount(() => {
-    getName();
-    getIntroduction();
-    getDescription();
+  //   onMount(() => {
+  //     getName();
+  //     getIntroduction();
+  //     getDescription();
+  //   });
+
+  onMount(async () => {
+    loading = true;
+    await Promise.all([getName(), getIntroduction(), getDescription()]);
+    loading = false; // Set loading to false when data is fetched
   });
+
+  // async function getData() {
+  //   // Simultaneously fetch data (similar to your previous functions)
+  //   await Promise.all([getName(), getIntroduction(), getDescription()]);
+  //   loading = false;
+  // }
+
+  // onMount(() => {
+  //   getData();
+  // });
 </script>
 
-<div class="main font-anek-devanagari">
-  <div
-    class="px-5 lg:px-10 py-5 h-full min-h-screen flex flex-col justify-between bg-gradient-to-tr from-zinc-400 via-neutral-300 to-gray-100"
-  >
-    <div class="flex flex-wrap items-end"></div>
+{#if loading}
+  <LoadingDots text="loading" />
+{:else}
+  <div class="main font-anek-devanagari">
+    <div
+      class="px-5 lg:px-10 py-5 h-full min-h-screen flex flex-col justify-between bg-gradient-to-tr from-zinc-400 via-neutral-300 to-gray-100"
+    >
+      <div class="flex flex-wrap items-end"></div>
 
-    <div class="flex flex-col justify-end gap-5">
-      <h1>
-        <div class="text-3xl">{myIntro}</div>
-        <div class="font-dm-serif text-6xl">{myName}</div>
-      </h1>
+      <div class="flex flex-col justify-end gap-5">
+        <h1>
+          <div class="text-3xl">{myIntro}</div>
+          <div class="font-dm-serif text-6xl">{myName}</div>
+        </h1>
 
-      <div>
-        <p class="text-3xl">{myDescription}</p>
-      </div>
-      <div class="flex flex-col justify-center items-center">
-        <p class="font-bold text-xl">Scroll down to see more (coming soon)</p>
-        <!-- <ArrowDownwardIcon /> -->
+        <div>
+          <p class="text-3xl">{myDescription}</p>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <p class="font-bold text-xl">Scroll down to see more (coming soon)</p>
+          <!-- <ArrowDownwardIcon /> -->
+        </div>
       </div>
     </div>
   </div>
-</div>
+{/if}
 
 <style>
   .main {
