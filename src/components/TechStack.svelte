738e30869b830f69ff468fe63f088a8ea7fbd999
fairuz <sakhiyfairuz@gmail.com>
2024-11-12 10:06:27 +0700

    [add] tech stack card

---
+++ src/components/TechStack.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let name: string;
+  export let imgUrl: string;
+</script>
+
+<div
+  class="border rounded-md flex flex-col h-28 w-28"
+>
+  <div class="flex-grow flex items-center justify-center">
+    <img
+      src={`${import.meta.env.VITE_CMS_STORAGE}${imgUrl}`}
+      alt={name}
+      class="w-16"
+    />
+  </div>
+  <p class="mt-2 text-center break-words">{name}</p>
+</div>
